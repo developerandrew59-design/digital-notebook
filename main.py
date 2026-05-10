56c78c5c84ba8e3194beb40b3d9f010c8f431b2b
developerandrew59-design <developerandrew59@gmail.com>
2026-05-10 16:29:46 +0530

    removed the psycopg2 lag

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
 from fastapi import Depends, FastAPI,HTTPException,status,Response
-import psycopg2
-from psycopg2.extras import RealDictCursor
+
 import time
 import models
 from database import engine, get_db
 from sqlalchemy.orm import Session
 from routers import notes,users,auth
 
-#while True:
-#    try:
-#        conn=psycopg2.connect( host="db",
-#        database="mydb",
-#        user="postgres",
-#        password="mysecretpassword",
-#        port=5432,
-#        cursor_factory=RealDictCursor)
-
-#        cursor=conn.cursor()
-#        print("Database connection was sucessful")
-#        break
-#    except Exception as error:
-#        print("connection to database failed")
-#        print("Error",error)
-#        time.sleep(2)    
-
-#models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(notes.router)
